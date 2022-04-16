fa19c928ce39a3792df908dca2d3cd89a9d0f01e
ASHOKROCK <93025555+ASHOKROCK@users.noreply.github.com>
2022-04-16 12:41:13 +0530

    Create sonar-properties.js

---
+++ sonar-properties.js
@@ -0,0 +1,8 @@
+'sonar.projectDescription': 'This is a Node JS application',
+'sonar.login': 'a5c51557120d935b5b880e80b9cfbbf6c1473cb3',
+  'sonar.host.url':'http://34.116.206.130:9000/'
+      'sonar.projectKey':'simple-reactjs-app'
+      'sonar.language':'js',
+      'sonar.sourceEncoding':'UTF-8',
+      'sonar.sources': '.',
+      'sonar.projectVersion':'0.1.0',
